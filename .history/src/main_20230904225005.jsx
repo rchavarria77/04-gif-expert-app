bd3cd2c12a9dcde4d164a7a87ceae8f49261b328
Roberto Chavarria Vega <rchavarria77@outlook.com>
2023-09-04 23:10:07 -0600

    Delete .history/src directory

--- .history/src/main_20230904225005.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { GifExpertApp } from "./GifExpertApp";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <GifExpertApp />
-  </React.StrictMode>
-);
